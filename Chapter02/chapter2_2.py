c0e44906da28d9bf83211b9d893189f1db03ab7c
caot <>
2021-05-20 04:06:33 -0400

    updated<M-C-S-F3>

--- Chapter02/chapter2_2.py
+++ Chapter02/chapter2_2.py
@@ -3,32 +3,29 @@
 import getpass
 from pexpect import pxssh
 
-devices = {'iosv-1': {'prompt': 'iosv-1#', 'ip': '172.16.1.20'}, 
-           'iosv-2': {'prompt': 'iosv-2#', 'ip': '172.16.1.21'}}
+devices = {
+    'iosv-1': {'prompt': 'iosv-1#', 'ip': '192.168.1.250'},
+    # 'iosv-2': {'prompt': 'iosv-2#', 'ip': '172.16.1.21'}
+}
+
 commands = ['term length 0', 'show version', 'show run']
 
 username = input('Username: ')
 password = getpass.getpass('Password: ')
 
 # Starts the loop for devices
-for device in devices.keys(): 
+for device in devices.keys():
     outputFileName = device + '_output.txt'
     device_prompt = devices[device]['prompt']
     child = pxssh.pxssh()
-    child.login(devices[device]['ip'], username.strip(), password.strip(), auto_prompt_reset=False)
+    child.login(devices[device]['ip'], username.strip(), password.strip(), sync_multiplier=20, auto_prompt_reset=False)
+
     # Starts the loop for commands and write to output
     with open(outputFileName, 'wb') as f:
         for command in commands:
             child.sendline(command)
             child.expect(device_prompt)
             f.write(child.before)
-    
+
     child.logout()
-
-
-    
-
-
-    
-    
 
